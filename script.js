b8078ca6b93a7e28e30d911f4a711d7c089aff19
Darryl <le.roux.darryl@gmail.com>
2021-03-02 21:06:26 +0200

    Added the spread operator

--- script.js
+++ script.js
@@ -36,8 +36,15 @@ const restaurant = {
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
 };
 
+/*
 // CALLING FUNCTION WITH OBJECT AS ARGUMENT
 restaurant.orderDelivery({
   time: '22:30',
@@ -51,6 +58,8 @@ restaurant.orderDelivery({
   address: 'Via Del Sole, 21',
   starterIndex: 1,
 });
+
+
 /////////////////////////////////////
 /////// DESTRUCTURING OBJECTS ///////
 /////////////////////////////////////
@@ -89,37 +98,88 @@ console.log(o, c);
 /////// DESTRUCTURING ARRAYS ///////
 ////////////////////////////////////
 
-// const arr = [2, 3, 4];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
-// // Destructuring an array
-// const [x, y, z] = arr;
-// console.log(x, y, z);
+// Destructuring an array
+const [x, y, z] = arr;
+console.log(x, y, z);
 
-// // You can rename the "variable" whatever you like. It will go per the indexing. So the first two indexes can be called - first, second. This can be called whatever you want.
-// const [first, second] = restaurant.categories;
-// console.log(first, second); // Prints the restaurant.categories[0] and r.s[1]
+// You can rename the "variable" whatever you like. It will go per the indexing. So the first two indexes can be called - first, second. This can be called whatever you want.
+const [first, second] = restaurant.categories;
+console.log(first, second); // Prints the restaurant.categories[0] and r.s[1]
 
-// // Skip an index in an array - just leave a blank space
-// let [main, , secondary] = restaurant.categories;
+// Skip an index in an array - just leave a blank space
+let [main, , secondary] = restaurant.categories;
 
-// // Switching variables - main, secondary to sedondary, main
-// [main, secondary] = [secondary, main];
-// console.log(main, secondary);
+// Switching variables - main, secondary to sedondary, main
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
 
-// // Receive two return values from a function
-// const [starter, mainCourse] = restaurant.order(2, 0);
-// console.log(starter, mainCourse);
+// Receive two return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
 
-// // Nested destructuring arrays
-// const nested = [2, 4, [5, 6]];
-// // const [i, , j] = nested;
-// // console.log(i, j);
-// const [i, , [j, k]] = nested;
-// console.log(i, j, k);
+// Nested destructuring arrays
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
 
-// // Default value for variables when destructuring
-// const [p = 1, q = 1, r = 1] = [8, 9];
-// console.log(p, q, r);
+// Default value for variables when destructuring
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+*/
+
+////////////////////////////////////
+///////// SPREAD OPERATOR //////////
+////////////////////////////////////
+
+const arr = [7, 8, 9];
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArray);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr); // [1, 2, 7, 8, 9]
+
+console.log(...newArr); // 1, 2, 7, 8, 9 - not as an array, but their individual numbers
+
+// Add a value to an array with the spread operator CREATING a new array
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join arrays together
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables arrays, strings, maps and sets but NOT OBJECTS
+const str = 'Jonas';
+const letters = [...str, ' ', 'S'];
+console.log(letters);
+console.log(...str);
+
+///// REAL WORLD EXAMPLE ////
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+
+//restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundIn: 1998, ...restaurant, founder: 'Guiseppi' };
+console.log(newRestaurant);
+
+// Shallow copies
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Italian Name yo';
+console.log(restaurantCopy, restaurant);
