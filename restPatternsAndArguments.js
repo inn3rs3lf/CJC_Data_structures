e8b6df698a8f7c7aa13171ea5251b4d3710d8bb9
Darryl <le.roux.darryl@gmail.com>
2021-06-27 18:35:37 +0200

    Finished rest pattern

--- restPatternsAndArguments.js
+++ restPatternsAndArguments.js
@@ -41,4 +41,55 @@ const restaurant = {
       `Here is your delicious pasta with ${ingredient1}, ${ingredient2}, ${ingredient3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
 };
+
+//////////////////////////////////////
+/////////// REST PATTERN /////////////
+//////////////////////////////////////
+
+// REST - as in the REST of the array unused
+
+// 1. DESTRUCTURING
+
+// SPREAD on the RIGHT side of =
+const arr = [1, 2, [3, 4]];
+// REST, because of LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// REST does not include skipped elements in an array.
+// REST must always be the last element
+const [pizza, , rissoto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, rissoto, otherFood);
+
+// Objects
+const { sat, ...weekDays } = restaurant.openingHours;
+console.log(sat, weekDays);
+
+// 2. FUNCTIONS
+// The arguments can be as many as we would like
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+// Should take however many arguments we want
+add(2, 3);
+add(5, 3, 7, 2);
+add(2, 6, 7, 8, 9, 2, 3);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushroom', 'spinach', 'olives', 'cheese');
+restaurant.orderPizza('mushroom');
