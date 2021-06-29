2c3d8b4e05c1dd219fdfd5d7ec3fdcb468d8132e
Darryl <le.roux.darryl@gmail.com>
2021-06-29 19:11:22 +0200

    Added looping for property names, values and keys

--- loopingObjects.js
+++ loopingObjects.js
@@ -55,3 +55,27 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties); // ['thur', 'fri', 'sat']
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of Object.keys(openingHours)) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close as ${close}`);
+}
