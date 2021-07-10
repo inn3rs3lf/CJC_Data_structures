33e41341168096c7014e10e28e81f71ef8eff7c9
Darryl <le.roux.darryl@gmail.com>
2021-07-10 22:07:05 +0200

    Looping over sets

--- setsmaps.js
+++ setsmaps.js
@@ -30,4 +30,12 @@ console.log(ordersSet.has('Bread')); // false
 // We can add and delete a value to a Set
 ordersSet.add('Garlic Bread');
 ordersSet.delete('Risotto');
+// ordersSet.clear(); // removes the entire set
 console.log(ordersSet);
+
+// You cannot retreive an item from a set like you would be able to do with arrays arr = [item[0]].
+// Due to the fact that the set wouldn't have any duplicates, you are able to check if the set has the item by using the .has() method on it.
+// If you need tos tore data in a specific order, then an array would be the better option.
+
+// You can loop over Sets the same as you can with arrays
+for (const order of ordersSet) console.log(order);
