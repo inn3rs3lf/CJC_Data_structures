715511340c9d6ebb8a337fb35af666de10776f73
Darryl <le.roux.darryl@gmail.com>
2021-07-10 22:17:12 +0200

    Finding values and size

--- setsmaps.js
+++ setsmaps.js
@@ -39,3 +39,23 @@ console.log(ordersSet);
 
 // You can loop over Sets the same as you can with arrays
 for (const order of ordersSet) console.log(order);
+
+// The main use case for Sets is to remove duplicate values in arrays!
+const staff = ['waiter', 'chef', 'waiter', 'manager', 'chef', 'waiter'];
+// We would like to know what positions we have at the restaurant
+// Create a new Set with the staff array
+// Because arrays are iterable, we can convert the Set to an array with the unique values
+// We can add the new Set and deconstruct it at the same time. The deconstruction is taking the Set, and by using the spread operator, we can destruct the set into array values
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique); // ['waiter', 'chef', 'manager']
+
+// If we wanted to know the size of the array with the unique items only
+// We can pass the entire array into the new Set, and then use the .size on it
+console.log(
+  new Set(['waiter', 'chef', 'waiter', 'manager', 'chef', 'waiter']).size
+);
+// Can also be written as such with the array in a variable
+console.log(new Set(staff).size);
+
+// You can also use it to see how many letters there are in a string, as a string is an iterable
+console.log(new Set('Darryl').size); // 5
