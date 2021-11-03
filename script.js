dae2769c70d9122de3ea495123a88b4e97684c3f
Darryl <le.roux.darryl@gmail.com>
2021-11-03 21:53:58 +0200

    Destructuring Objects

--- script.js
+++ script.js
@@ -6,38 +6,76 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focacia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 12,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
 
-// DESTRUCTURING
-const arr = [2, 3, 4];
-const [x, y, z] = arr;
-console.log(x, y, z); // 2, 3, 4
+// DESTRUCTURING OBJECTS
+const { name, openingHours, categories } = restaurant;
+console.log(openingHours);
 
-// const [first, second] = restaurant.categories;
-let [main, , secondary] = restaurant.categories; // the , , will skip the element
-console.log(main, secondary); // 'Italian', 'Pizzeria'
+// Change key to a different variable
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
 
-// With destructuring you can simply reasign as such
-[main, secondary] = [secondary, main];
-console.log(main); // 'Vegatarian'
+// Setting defaults if a key doesn't exist
+const { menu = [], starterMenu = [] } = restaurant;
+console.log(menu, starterMenu); // menu will now be an empty array
 
-// Return the order from a function
-console.log(restaurant.order(2, 0));
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(`Your order is ${starter} with your ${mainCourse}`);
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
 
-// Nested arrays with destructuring
-const nested = [2, 4, [5, 6]];
-const [i, , j] = nested;
-console.log(i, j);
-//Destructuring within destructuring
-const [a, , [c, d]] = nested;
-console.log(a, c, d);
+({ a, b } = obj);
+console.log(a, b);
 
-// Setting the default values incase the array is shorter than we think
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r); // It does not overwite the actual array value
+// // DESTRUCTURING ARRAYS
+// const arr = [2, 3, 4];
+// const [x, y, z] = arr;
+// console.log(x, y, z); // 2, 3, 4
+
+// // const [first, second] = restaurant.categories;
+// let [main, , secondary] = restaurant.categories; // the , , will skip the element
+// console.log(main, secondary); // 'Italian', 'Pizzeria'
+
+// // With destructuring you can simply reasign as such
+// [main, secondary] = [secondary, main];
+// console.log(main); // 'Vegatarian'
+
+// // Return the order from a function
+// console.log(restaurant.order(2, 0));
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(`Your order is ${starter} with your ${mainCourse}`);
+
+// // Nested arrays with destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// //Destructuring within destructuring
+// const [a, , [c, d]] = nested;
+// console.log(a, c, d);
+
+// // Setting the default values incase the array is shorter than we think
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r); // It does not overwite the actual array value
